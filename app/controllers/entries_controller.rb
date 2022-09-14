f70554dddfff5b93ad758a8c1e6cf9b4be6cdcf8
oliwia <oliwiag157@gmail.com>
2022-09-14 11:05:24 +0200

    second commit idk which day

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,9 +1,18 @@
 class EntriesController < ApplicationController
 def index
-  @entries = Entry.all.sort_by{|entry| entry.date}.reverse
+  @entries = Entry.all.sort_by{|entry| entry.title.downcase}
 end
 
 def show
   @entry = Entry.find(params["id"])
+end
+
+def new
+end
+
+def create
+  entry_params = params["entry"].permit("title", "contents")
+  entry = Entry.create(entry_params)
+  redirect_to(entry_path(entry))
 end
 end
