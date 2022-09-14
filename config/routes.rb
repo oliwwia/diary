19816cb1d25aaecb1942f50ad7cff1d737300e99
oliwia <oliwiag157@gmail.com>
2022-09-14 10:20:24 +0200

    first commit idk which day'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "welcome#index"
   get 'welcome/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   resources "entries"
+  #get "/entries/:test", to: "entries#show"
 end
